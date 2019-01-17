0a750263bc62528de2228f4e998d2def6b4e3f77
Steve Mustanski <steve.mustanski@gmail.com>
2019-01-16 20:19:40 -0500

    added 3 draft cards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Jumbotron} from 'reactstrap';
+import {
+  Jumbotron, Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button, Row, Col, Container
+} from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Jumbotron className="bg-info">
-        <h1>Checkout the reactstrap jumbotron!</h1>
-        <p className="lead">Just a simple jumbotron to start things off.</p>
-      </Jumbotron>
+        <Jumbotron className="bg-info">
+          <h1>Checkout the reactstrap jumbotron!</h1>
+          <p className="lead">Just a simple jumbotron to start things off.</p>
+        </Jumbotron>
+
+        <div>
+
+        </div>
+        <Container>
+          <Row>
+            <Col xs="6" sm="4">
+              <Card>
+                <CardImg top width="30%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xs="6" sm="4">
+              <Card>
+                <CardImg top width="30%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xs="6" sm="4">
+              <Card>
+                <CardImg top width="30%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
 
       </div>
     );
